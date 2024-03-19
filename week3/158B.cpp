9b7b9dd97d293bdc8a15a1a17e334499ced7cbb9
msu0922 <58171852+msu0922@users.noreply.github.com>
2024-03-19 10:50:04 +0000

    week3

--- week3/158B.cpp
+++ week3/158B.cpp
@@ -1,7 +1,58 @@
 // https://codeforces.com/problemset/problem/158/B
 
+#include <iostream>
+
+using namespace std;
+
 int main() {
+    int n, cnt;
+    cin >> n;
+
+    int groups[5];
+
+    // 4
+    for (int i = 0; i < n; i++) {
+        cin >> groups[i];
+        if (groups[i] == 4)
+            cnt += 1;
+    }
+
+    // 3 & 1
+    int threes, ones;
+
+    for (int i = 0; i < n; i++) {
+        if (groups[i] == 3)
+            threes += 1;
+        else if (groups[i] == 1)
+            ones += 1;
+    }
+
+    if (threes / ones > 0) { // 3의 개수 > 1의 개수
+        cnt += threes;
+        ones -= threes; // 3+1 맞추고 남은 1의 개수
+
+        // 2
+        int twos;
+
+        for (int i = 0; i < n; i++) {
+            if (groups[i] == 2)
+                twos += 1;
+        }
+    } else { // 3의 개수 < 1의 개수
+        cnt += ones;
+        threes -= ones; // 3+1 맞추고 남은 3의 개수
+
+
+    }
+
     
+    // 2
+    int twos;
+
+    for (int i = 0; i < n; i++) {
+        if (groups[i] == 2)
+            twos += 1;
+    }
 
     return 0;
 }
--- week3/734B.cpp
+++ week3/734B.cpp
@@ -1,6 +1,13 @@
 // https://codeforces.com/problemset/problem/734/B
 
+#include <iostream>
+
+using namespace std;
+
 int main() {
+    int k2, k3, k5, k6;
+
+    cin >> k2 >> k3 >> k5 >> k6;
     
 
     return 0;
