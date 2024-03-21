d7856434e814f1568ace2adc44657ff311573b5b
msu0922 <58171852+msu0922@users.noreply.github.com>
2024-03-21 15:18:42 +0900

    self

---
+++ self/10E.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/problemset/problem/10/E
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n = 0;
+    cin >> n;
+
+    int val[n] = {0};
+
+    
+
+    for (int i = 0; i < n; i++) {
+        
+    }
+
+    return 0;
+}
