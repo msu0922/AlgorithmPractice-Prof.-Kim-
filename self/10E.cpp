711f48ec90746130196794af3a0733cbf60a5cfa
msu0922 <58171852+msu0922@users.noreply.github.com>
2024-03-21 07:14:00 +0000

    self

--- self/10E.cpp
+++ self/10E.cpp
@@ -1,20 +1,37 @@
-// https://codeforces.com/problemset/problem/10/E
+// https://codeforces.com/problemset/problem/13/A
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    int n = 0;
-    cin >> n;
-
-    int val[n] = {0};
-
+    int A = 0;
+    cin >> A;
     
+    int base[A+1];
 
-    for (int i = 0; i < n; i++) {
-        
+    for (int i = 2; i < A; i++) {
+        int base10 = A;
+        int sum = 0;
+
+        while (base10 >= i) {
+            sum += base10 % i;
+            base10 /= i;
+        }
+        sum += base10;
+        base[i] = sum;
     }
+
+    // 분수 출력
+    int total = 0;
+    
+    for (int i = 2; i < A; i++)
+        total += base[i];
+
+    int gcd = __gcd(total, A-2);
+
+    cout << total/gcd << "/" << (A-2)/gcd << endl;
 
     return 0;
 }
